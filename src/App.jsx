6edc80b8a66427f1e1117c70f220152f38ccafb4
pradeep800 <pradeep8b0@gmail.com>
2024-01-30 14:46:44 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ const a=["animate-slideIn","animate-slideOut"]
 function App() {
   const ref=useRef();
   const [open,setOpen]=useState(false);
+  const [firstTime,setFirstTime]=useState(true);
+  const [addOneTimeClass,setAddOneTimeClass]=useState(true);
 useEffect(()=>{
-  console.log("inside effect")
-  if(open){
+  if(firstTime){
+    setFirstTime(false);
+    return;
+  }
+  else if(open){
     ref.current.classList.remove("animate-slideOut")
     ref.current.classList.add("animate-slideIn")
-    console.log("On")
-    
   }else{
     ref.current.classList.remove("animate-slideIn")
     ref.current.classList.add("animate-slideOut");
+ 
   }
 },[open])
   return (
@@ -22,10 +26,12 @@ useEffect(()=>{
     <div  onClick={()=>{
       console.log("hello world")
       setOpen(!open)
+         if(addOneTimeClass)
+    setAddOneTimeClass(false);
       }} >|||</div>
     
     <div className=" " >
-         <ol ref={ref} className={`fixed   bg-green-300 w-[100%] h-[100%] flex items-center justify-center ${open?"-translate-x-[100%]":"translate-x-0"}`}>
+         <ol ref={ref} className={`fixed   bg-green-300 w-[100%] h-[100%] flex items-center justify-center ${open?"-translate-x-[100%]":"translate-x-0"} ${addOneTimeClass?"!translate-x-[-100%]":""} `}>
   <li>list1</li>
     <li>list2
       <li>list3</li>
